c99c516fb83997ccd058dd6b50fb7ede870a926e
Faaiz Mahmood <faizzafar44@gmail.com>
2025-08-30 22:14:01 +0500

    update title + favicon

--- src/pages/ComingSoonLanding.tsx
+++ src/pages/ComingSoonLanding.tsx
@@ -3,8 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
-import gif1 from '../assets/imgs/Lip_Animation.gif'
-import gif2 from '../assets/imgs/Character_Animation.gif'
+import gif1 from '../assets/gifs/Lip_Animation.gif'
+import gif2 from '../assets/gifs/Character_Animation.gif'
 
 const ComingSoonLanding = () => {
   return (
