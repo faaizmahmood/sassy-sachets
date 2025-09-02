e2c1295a65f556bf3553535c6ba86de2a5ee3700
Faaiz Mahmood <faizzafar44@gmail.com>
2025-09-02 21:58:18 +0500

    update animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
 import ComingSoonLanding from './pages/ComingSoonLanding'
+import { motion } from "framer-motion";
 
 function App() {
 
 
   return (
     <>
-      <ComingSoonLanding />
+
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.3 }}
+      >
+
+        <ComingSoonLanding />
+
+
+      </motion.main>
+
     </>
   )
 }
